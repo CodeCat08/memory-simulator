4c2608caeaa17811929ecda021f9d45032e937ba
David <27027506+CodeCat08@users.noreply.github.com>
2021-08-16 20:18:10 -0400

    Update BitUtilities.h

--- BitUtilities.h
+++ BitUtilities.h
@@ -3,10 +3,6 @@
 //	Project:		GroupProject
 //	File Name:		PageTable.cpp
 //	Description:    Bit stuff
-//	Course:			CSCI-4727-940: Operating Systems
-//	Author:			Jordan Cavins, Department of Computing, East Tennessee State University
-//	Created:		Wednesday, December 9 2020
-//	Copyright:	    Jordan Cavins 2020
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
